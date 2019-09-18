01ec0ae23ee7583564b94e32793e1976c0f38380
jod35 <jodestrevin@gmail.com>
2019-09-18 11:41:29 -0400

    added multiple urls

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home,about,services
 
 urlpatterns = [
     path('', home),
+    path('about/', about),
+    path('services',services),
     path('admin/', admin.site.urls),
     
 ]
--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -2,3 +2,9 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Hello</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About</h1>")
+
+def services(request):
+    return HttpResponse("<h1>Services</h1>")
