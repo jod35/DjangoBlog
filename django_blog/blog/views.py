c372a6c519436906029925337384b414293e7d51
jod35 <jodestrevin@gmail.com>
2019-09-19 12:10:46 -0400

    Worked on the templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,8 +2,31 @@ from django.shortcuts import render
 from  django.http import HttpResponse
 # Create your views here.
 
+
+posts=[
+    {
+        'author':"Ssali Jonathan",
+        'title':"Post One",
+        'content':" Post Content",
+        'date_posted':"17th September 2019" 
+    },
+    {
+        'author':"Kaweesa Andrew",
+        'title':"Post Two",
+        'content':" Post Content",
+        'date_posted':"18th September 2019" 
+    }
+]
 def home(request):
-    return HttpResponse("<h1>Hello Home</h1>")
+    context={
+        'posts' : posts
+    }
+    return render(request,'blog/index.html',context=context)
 
 def about(request):
-    return HttpResponse('<h1>This is the About Page</h1>')
+    context={
+        'title': "About Page",
+        'posts' : posts
+    }
+    return render(request,'blog/about.html',context)
+
